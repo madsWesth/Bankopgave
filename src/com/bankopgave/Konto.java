9d763d6b2416d477e12dc1c5b323618df2900284
FrederikMeincke <meincke.frederik@gmail.com>
2020-10-27 15:29:08 +0100

    Test 1 - ændret i Konto

--- src/com/bankopgave/Konto.java
+++ src/com/bankopgave/Konto.java
@@ -1,4 +1,35 @@
 package com.bankopgave;
 
 public class Konto {
+
+    private int kontonummer;
+    private String ejernavn;
+    private double saldo;
+
+    public int getKontonummer() {
+        return kontonummer;
+    }
+
+    public void setKontonummer(int kontonummer) {
+        this.kontonummer = kontonummer;
+    }
+
+    public String getEjernavn() {
+        return ejernavn;
+    }
+
+    public void setEjernavn(String ejernavn) {
+        this.ejernavn = ejernavn;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+
+
 }
